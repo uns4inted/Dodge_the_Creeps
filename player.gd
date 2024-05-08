9a14a8f6144728babb692151adfdf96a2683bd49
Uns4inted <lalanskyl@gmail.com>
2024-05-08 08:21:55 +0300

    Update Player functionality: movement and animation control; Add movement keybindings;

---
+++ player.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+
+@export var speed = 400; # pixels/sec
+var screen_size;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size;
+	hide();
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# 1.check for input
+	var velocity = Vector2.ZERO; # players movement vector
+	if (Input.is_action_pressed("move_up")):
+		velocity.y -= 1;
+	if (Input.is_action_pressed("move_down")):
+		velocity.y += 1;
+	if (Input.is_action_pressed("move_left")):
+		velocity.x -= 1;
+	if (Input.is_action_pressed("move_right")):
+		velocity.x += 1;
+	
+	# 2.move in given direction
+	if (velocity.length() > 0):
+		velocity = velocity.normalized() * speed;
+		$AnimatedSprite2D.play();
+	else:
+		$AnimatedSprite2D.stop(); 
+	
+	position += velocity * delta;
+	position = position.clamp(Vector2.ZERO, screen_size); #prevent player from leaving screen
+
+	# 3.play the appropriate animation
+	if (velocity.x != 0):
+		$AnimatedSprite2D.animation = "walk";
+		$AnimatedSprite2D.flip_v = false;
+		$AnimatedSprite2D.flip_h = velocity.x < 0;
+	if (velocity.y != 0):
+		$AnimatedSprite2D.animation = "up";
+		$AnimatedSprite2D.flip_v = velocity.y > 0;
+		
+	
