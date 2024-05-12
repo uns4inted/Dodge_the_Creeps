eb83309ce7a9f0526532b6294e21474dc59eb0ee
Uns4inted <lalanskyl@gmail.com>
2024-05-12 23:11:53 +0300

    Add hud to main scene, connect all scenes scripts together

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ var score;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game();
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +16,15 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop();
 	$MobTimer.stop();
+	$HUD.show_game_over();
 	
 func new_game():
 	score = 0;
 	$Player.start($StartPosition.position);
 	$StartTimer.start();
+	$HUD.update_score(score);
+	$HUD.show_message("Get Ready");
+	get_tree().call_group("mobs", "queue_free");
 
 
 func _on_mob_timer_timeout():
@@ -51,6 +55,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1;
+	$HUD.update_score(score);
 
 
 func _on_start_timer_timeout():
